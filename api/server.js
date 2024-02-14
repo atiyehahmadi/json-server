508ff074f8742c83fb9e9750a311d2e46d7365c5
atiyehahmadi <33490348+atiyehahmadi@users.noreply.github.com>
2024-02-14 17:45:55 +0330

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,8 +20,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-     '/blog/:resource/:id/show': '/:resource/:id'
-    '/categoryList/:resource': '/:resource'
+    '/api/categoryList': '/api/categoryList'
 }))
 server.use(router)
 server.listen(3000, () => {
